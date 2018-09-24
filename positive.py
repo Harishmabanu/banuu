6f44eb10f13de43f8a4cd35c532400335dea0e4f
Harishmabanu <42801646+Harishmabanu@users.noreply.github.com>
2018-09-24 01:40:12 -0700

    Update positive.py

--- positive.py
+++ positive.py
@@ -1,7 +1,7 @@
-n = float(input("Enter a number: "))
+n=float(input("Enter a num: "):
 if n > 0:
-   print("Positive number")
-elif n == 0:
+   print("Positive num")
+elif n==0:
    print("Zero")
-else:
-   print("Negative number")
+ else:
+   print("Negative num")
