4357017b44b50949f5598a30c60fd74c3c986409
Harishmabanu <42801646+Harishmabanu@users.noreply.github.com>
2018-09-24 21:37:39 -0700

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,8 @@
+n=input("enter: ")
+if n in('a','e','i','o','u'):
+	print("%s is a vowel." %l)
+elif n=='y':
+	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+else:
+	print("%s is a consonant." %l) 
+	
