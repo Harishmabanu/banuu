cb3554551dec926434f9a61c81bf3e36943a2e63
Harishmabanu <42801646+Harishmabanu@users.noreply.github.com>
2018-09-25 23:10:50 -0700

    Create natural.py

---
+++ natural.py
@@ -0,0 +1,12 @@
+print("Enter '0' for exit");
+num = int(input("sum of natural number:"));
+if num==0:
+    exit();
+elif num<1:
+    print("enter a positive number..exiting..");
+else:
+    sum=0;
+    while num>0:
+    	sum +=num;
+    	num -=1;
+    print(sum);
