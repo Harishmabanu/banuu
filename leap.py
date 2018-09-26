29c51716153f5f0c26abc081e040c160ee97d69e
Harishmabanu <42801646+Harishmabanu@users.noreply.github.com>
2018-09-25 22:40:28 -0700

    Create leap.py

---
+++ leap.py
@@ -0,0 +1,16 @@
+num = input("Enter year:");
+if num=='x':
+    exit();
+try:
+    year=int(num);
+except ValueError:
+    print("enter the year...existing");
+else:
+    if((year%4==0) and (year%100!=0)):
+        print(year, "is a Leap Year.");
+    elif((year%100==0) and (year%400==0)):
+    	print(year,"is a Leap Year.");
+    elif(year%400==0):
+    	print(year,"is a Leap Year");
+    else:
+    	print(year,"is not a Leap Year");
